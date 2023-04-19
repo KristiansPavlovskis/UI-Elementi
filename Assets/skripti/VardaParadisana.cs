a2f4386ac9bb0b14c0070b5ae2153b1984699772
Pavlovskis <kristianspavlovskis@gmail.com>
2023-04-19 12:21:39 +0300

    Sagatavots viss pirmā skripta rakstīšanai

---
+++ Assets/skripti/VardaParadisana.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VardaParadisana : MonoBehaviour {
+	public int kadrSk;
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Uzsākta programma! tu esi "+kadrSk+" kadrā");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("kadrs" + kadrSk);
+		kadrSk++;
+	}
+}
